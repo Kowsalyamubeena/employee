0a6946dbe73e14888b79153e7110fe870ac9d6a5
Kowsalyamubeena <kowsalyaramesh1998@gmail.com>
2023-10-20 10:49:33 +0530

    Add Emp

--- src/main/java/com/employee/entity/Employee.java
+++ src/main/java/com/employee/entity/Employee.java
@@ -18,6 +18,14 @@ public class Employee {
 	private String gender;
 	private int salary;
 	private String shift;
+	private int values;
+	
+	public int getValues() {
+		return values;
+	}
+	public void setValues(int values) {
+		this.values = values;
+	}
 	public int getId() {
 		return id;
 	}
